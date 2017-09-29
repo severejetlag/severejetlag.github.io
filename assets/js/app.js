5e1867ccac423cabf342c695635fc7c1060eee5f
severejetlag <nhl1013@gmail.com>
2017-09-29 13:32:36 -0700

    Added smooth scrolling functionality to nav bar clicks in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,17 @@ console.log("app.js linked.");
 //start doc ready
 $(function() {
 	var navHeight = $("#header").height();
-	console.log(navHeight)
 	$("#main-content").css("padding-top",navHeight + "px");
+
+	// Creating scroll functionality 
+	var $navItems = $('#main-nav a');
+	$navItems.on('click',function navScroll(event){
+		event.preventDefault();
+		$scrollDestination = $($(this).attr('href'));
+		$scrollAmount = $scrollDestination.position().top - navHeight;
+		$('html,body').animate({
+			scrollTop:$scrollAmount
+		}, 500);
+	})
 
 });//end doc ready
