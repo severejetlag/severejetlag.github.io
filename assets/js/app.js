213fc507fba789243e71ba623e909ecf45e4879f
severejetlag <nhl1013@gmail.com>
2017-09-29 09:09:40 -0700

    Created sticky nav and added footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ console.log("app.js linked.");
 
 //start doc ready
 $(function() {
-
+	var navHeight = $("#header").height();
+	console.log(navHeight)
+	$("#main-content").css("padding-top",navHeight + "px");
 
 });//end doc ready
