0ebd726c3ec15aa06d9b0293811b53370d367869
severejetlag <nhl1013@gmail.com>
2018-01-09 14:29:37 -0500

    updating to pull from github API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,20 @@
 // sanity check
 console.log("app.js linked.");
 
+var ajaxParams = {
+	method: 'GET',
+	url:'https://api.github.com/users/severejetlag/repos',
+	dataType: "jsonp",
+	success: onGitSuccess,
+	error: onGitError
+}
+
 //start doc ready
 $(function() {
 	var navHeight = $("#header").height();
 	$("#main-content").css("padding-top",navHeight + "px");
 
-	// Creating scroll functionality 
+	// Creating scroll functionality
 	var $navItems = $('#main-nav a');
 	$navItems.on('click',function navScroll(event){
 		event.preventDefault();
@@ -14,7 +22,26 @@ $(function() {
 		$scrollAmount = $scrollDestination.position().top - navHeight;
 		$('html,body').animate({
 			scrollTop:$scrollAmount
-		}, 500);
+		}, 1000);
 	})
-
+	$.ajax(ajaxParams);
 });//end doc ready
+
+function onGitSuccess(json){
+	json.data.forEach(function(repo, i){
+		console.log(repo);
+		if(!repo.archived){
+			$('#work ul').append(`
+				<li>
+					<a href="${repo.html_url}">
+						${repo.name}<i class="fa fa-code" aria-hidden="true"></i>
+					</a>
+				</li>
+				`)
+		}
+	})
+}
+
+function onGitError(){
+	console.log('sad');
+}
